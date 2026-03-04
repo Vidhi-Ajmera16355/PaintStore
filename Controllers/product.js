1a9555ec1b461d233b9d1ce35f467a1333e79e62
vidhi-hestabit <vidhia.hestabit@gmail.com>
2026-03-04 18:05:20 +0530

    correction in product controller

--- Controllers/product.js
+++ Controllers/product.js
@@ -4,7 +4,7 @@ import  Product from "../Models/Product.js";
 export const addProduct = async (req, res) => {
   const { title, description, price, category, qty, imgSrc } = req.body;
   try {
-    let product = await Products.create({
+    let product = await Product.create({
       title,
       description,
       price,
@@ -14,20 +14,20 @@ export const addProduct = async (req, res) => {
     });
     res.json({ message: "Product added successfully..!!", product });
   } catch (error) {
-    res.json(error.message);
+    res.status(500).json({ message: error.message });
   }
 };
 
 // get products
 export const getProducts = async (req, res) => {
-  let products = await Products.find().sort({ createdAt: -1 });
+  let products = await Product.find().sort({ createdAt: -1 });
   res.json({ message: "All Products", products });
 };
 
-// find product by ID
+// get by id
 export const getProductById = async (req, res) => {
   const id = req.params.id;
-  let product = await Products.findById(id);
+  let product = await Product.findById(id);
   if (!product) return res.json({ message: "Invalid Id" });
   res.json({ message: "Specific Product", product });
 };
